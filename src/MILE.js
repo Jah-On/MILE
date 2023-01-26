e98ad3743c79c8c488e1a87e204217e7cf325917
stefnotch <brandmairstefan@gmail.com>
2023-01-26 14:15:41 +0100

    Workaround for Vite bug

--- src/MILE.js
+++ src/MILE.js
@@ -1,7 +1,11 @@
 import { windowLeave, printOutput } from "./events.js"
 import { addNewInput, backToBase } from "./MILE_ui.js"
 
+// Main entry point
+
 window.addEventListener("beforeunload", windowLeave);
-document.getElementById("baseForm").addEventListener("submit", addNewInput);
-document.getElementById("backToBase").addEventListener("click", backToBase);
-document.getElementById("printButton").addEventListener("click", printOutput);
+window.addEventListener("load", () => {
+  document.getElementById("baseForm").addEventListener("submit", addNewInput);
+  document.getElementById("backToBase").addEventListener("click", backToBase);
+  document.getElementById("printButton").addEventListener("click", printOutput);
+});
